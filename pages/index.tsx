6c9414ec3fd14973dad10fa8569ed240f6fbac06
Ahljenn <ahljennm2@gmail.com>
2022-07-01 23:10:00 -0700

    Format all files w/ prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-    return (
-        <div>
-            <Head>
-                <title>Hilofy</title>
-                <link rel="icon" href="/logo.png" />
-            </Head>
+  return (
+    <div>
+      <Head>
+        <title>Hilofy</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
 
-            <Image src="/logo.png" width={50} height={50} />
-        </div>
-    );
+      <Image
+        className="object-contain"
+        src="/logo.png"
+        width={200}
+        height={100}
+      />
+    </div>
+  );
 };
 
 export default Home;
