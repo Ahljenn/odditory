6e5003e5486c6965203c25d185735315d8250dc9
Ahljenn <ahljennm2@gmail.com>
2022-07-11 00:47:28 -0700

    Styling for genres

--- pages/dashboard/genre.tsx
+++ pages/dashboard/genre.tsx
@@ -8,13 +8,25 @@ interface Props {}
 //   return arg[0].toUpperCase() + arg.slice(1);
 // };
 
+//Note - add API to fetch random image from genre
+
+//Add random colors for bg
+
 const genre: React.FC = (props: Props): JSX.Element => {
   return (
     <>
       <SubpageHeader pageName="Genre" />
-      <section>
+      <div className="flex justify-center mb-5">
+        <input placeholder="Search"></input>
+      </div>
+      <section className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-3 mx-5">
         {genres.map((genre: string) => (
-          <p key={genre}>{genre}</p>
+          <div
+            className="cursor-pointer hover:bg-slate-300 hover:text-slate-600 rounded-xl align-start bg-slate-600 gap-1 h-60"
+            key={genre}
+          >
+            <p className="text-2xl m-5">{genre[0].toUpperCase() + genre.slice(1)}</p>
+          </div>
         ))}
       </section>
     </>
