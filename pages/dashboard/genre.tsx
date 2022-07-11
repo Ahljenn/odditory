ba9711b8c17e28c17e686ff4e951caad4fcb6086
Ahljenn <ahljennm2@gmail.com>
2022-07-11 00:19:37 -0700

    Changed data in genres and render genre

--- pages/dashboard/genre.tsx
+++ pages/dashboard/genre.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { genres } from '../../lib/genres';
 import SubpageHeader from '../../components/ui/SubpageHeader';
 
 interface Props {}
+
+// const parse: string = (arg: string): string => {
+//   return arg[0].toUpperCase() + arg.slice(1);
+// };
 
 const genre: React.FC = (props: Props): JSX.Element => {
   return (
     <>
       <SubpageHeader pageName="Genre" />
+      <section>
+        {genres.map((genre: string) => (
+          <p key={genre}>{genre}</p>
+        ))}
+      </section>
     </>
   );
 };
