9f8ebf7b5c0070c24a12b66844b275b59003544d
Ahljenn <ahljennm2@gmail.com>
2022-07-12 00:07:31 -0700

    Added random colors to genres

--- pages/dashboard/genre.tsx
+++ pages/dashboard/genre.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { genres } from '../../lib/genres';
 import SubpageHeader from '../../components/ui/SubpageHeader';
 
 interface Props {}
 
-// const parse: string = (arg: string): string => {
-//   return arg[0].toUpperCase() + arg.slice(1);
-// };
-
 //Note - add API to fetch random image from genre
 
-//Add random colors for bg
+const colors: string[] = [
+  'bg-slate-500',
+  'bg-blue-500',
+  'bg-green-500',
+  'bg-orange-500',
+  'bg-red-500',
+  'bg-purple-500',
+  'bg-pink-500',
+  'bg-teal-500',
+  'bg-indigo-500',
+];
 
 const genre: React.FC = (props: Props): JSX.Element => {
   return (
@@ -19,13 +25,15 @@ const genre: React.FC = (props: Props): JSX.Element => {
       <div className="flex justify-center mb-5">
         <input placeholder="Search"></input>
       </div>
-      <section className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-3 mx-5">
+      <section className="grid grid-cols-2 sm:grid-cols-3 gap-3 2xl:gap-5 mx-5">
         {genres.map((genre: string) => (
           <div
-            className="cursor-pointer hover:bg-slate-300 hover:text-slate-600 rounded-xl align-start bg-slate-600 gap-1 h-60"
+            className={`cursor-pointer hover:bg-slate-300 hover:text-slate-600 rounded-xl align-start gap-1 h-60 ${
+              colors[Math.floor(Math.random() * colors.length)]
+            }`}
             key={genre}
           >
-            <p className="text-2xl m-5">{genre[0].toUpperCase() + genre.slice(1)}</p>
+            <p className="sm:text-2xl m-5">{genre[0].toUpperCase() + genre.slice(1)}</p>
           </div>
         ))}
       </section>
