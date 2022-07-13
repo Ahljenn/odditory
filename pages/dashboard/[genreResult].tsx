b8300c85200c9e199ecb90dd4a28804dc6cca904
Ahljenn <ahljennm2@gmail.com>
2022-07-13 00:16:39 -0700

    styling for genreResult responsive grids

--- pages/dashboard/[genreResult].tsx
+++ pages/dashboard/[genreResult].tsx
@@ -30,7 +30,7 @@ const genreResult: React.FC = (): JSX.Element => {
         try {
           const data = await spotifyApi.getRecommendations({
             seed_genres: [genreId],
-            limit: 22,
+            limit: 24,
           });
           setTracks(data.body.tracks);
           setLoaded(true);
@@ -42,67 +42,36 @@ const genreResult: React.FC = (): JSX.Element => {
   }, [spotifyApi]);
 
   console.log(tracks);
+  console.log(genreId);
 
   return (
     <>
       <SubpageHeader pageName={genreTitle} />
       <h1> Genre: {genreTitle} </h1>
       <h1> Id: {genreId} </h1>
-      <section className="mt-5 mx-5 grid grid-cols-2 sm:grid-cols-3 gap-4 md:grid-cols-4 xl:gap-7 max-w-screen-4xl justify-center">
+      <section className="mt-5 mx-5 grid grid-cols-2 sm:grid-cols-3 gap-4 md:grid-cols-4 2xl:grid-cols-6 xl:gap-7 max-w-screen-4xl justify-center">
         {isLoaded
           ? tracks.map((track: any, index: number) => (
-              <div
-                className={
-                  index === 0 || index == 11 ? 'col-span-1 xl:col-span-2 xl:row-span-2' : ''
-                }
-                key={index}
-              >
+              <div key={index}>
                 <img
                   src={track.album.images[0].url}
                   alt={track.name}
-                  className={
-                    index === 0 || index == 11
-                      ? 'track-primary sm:w-[50.5rem] xl:w-full xl:h-[94%]'
-                      : 'track-primary sm:w-[25.5rem] xl:w-[35rem]'
-                  }
+                  className="track-primary sm:w-[25.5rem] xl:w-[35rem]"
                 />
                 <div className="flex flex-col justify-center">
-                  <h1
-                    className={
-                      index === 0 || index === 15
-                        ? 'text-center xl:text-2xl whitespace-nowrap truncate'
-                        : 'text-center whitespace-nowrap truncate'
-                    }
-                  >
-                    {track.name}
-                  </h1>
-                  <h2
-                    className={
-                      index === 0 || index === 15
-                        ? 'text-center font-bold 2xl:text-2xl whitespace-nowrap truncate'
-                        : 'text-center font-bold whitespace-nowrap truncate'
-                    }
-                  >
+                  <h1 className="text-center whitespace-nowrap truncate">{track.name}</h1>
+                  <h2 className="text-center font-bold whitespace-nowrap truncate">
                     {track.artists[0].name}
                   </h2>
                 </div>
               </div>
             ))
           : tempGrid.map((temp, index) => (
-              <div
-                className={
-                  index === 0 || index == 15 ? 'col-span-1 xl:col-span-2 xl:row-span-2' : ''
-                }
-                key={index}
-              >
+              <div key={index}>
                 <img
-                  src="./logo.png"
+                  src="/logo.png"
                   alt="loading"
-                  className={
-                    index === 0 || index == 15
-                      ? 'track-primary sm:w-[50.5rem] 2xl:w-full 2xl:h-[95.5%] opacity-5'
-                      : 'track-primary sm:w-[25.5rem] 2xl:w-[35rem] opacity-5'
-                  }
+                  className="track-primary sm:w-[25.5rem] 2xl:w-[35rem] opacity-5"
                 />
               </div>
             ))}
