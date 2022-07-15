f49b80436eac7e1dc9e1adf5a6dcc11b1a313a8d
Ahljenn <ahljennm2@gmail.com>
2022-07-14 19:55:50 -0700

    Loading playlist tracks to dynamic route:

--- pages/playlist/[playlistResult].tsx
+++ pages/playlist/[playlistResult].tsx
@@ -12,13 +12,52 @@ const playlistResult: React.FC = (): JSX.Element => {
    * @returns JSX.Element - renders the playlist result component.
    */
   const router = useRouter();
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
   const { playlistImg, playlistId, playlistTitle } = router.query;
+  const [tracks, setTracks] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      (async () => {
+        try {
+          const data = await spotifyApi.getPlaylistTracks(playlistId);
+          setTracks(data.body.items);
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
+  }, [spotifyApi, playlistId, session]);
 
   console.log('1', playlistImg, playlistId);
 
   return (
     <>
       <SubpageHeader pageName="Playlist" />
+      <section className="flex justify-center bg-gradient-to-b to-secondary from-primary padding-8 w-full">
+        <div className="flex flex-col items-center">
+          <img className="w-[20rem] sm:w-[30rem] bg-slate-600 rounded-lg p-3" src={playlistImg} />
+          <h1 className="text-xl font-bold py-5 truncate">{playlistTitle}</h1>
+        </div>
+      </section>
+      <div>
+        {tracks &&
+          tracks.map((track: any) => {
+            return (
+              <div className="flex items-center gap-5 mb-5">
+                <img
+                  className="w-[5rem] bg-slate-600 rounded-lg p-1"
+                  src={track.track.album.images[0].url}
+                />
+                <div className="flex flex-col">
+                  <h1 className="text-xl truncate">{track.track.name}</h1>
+                  <h1 className="text-xl truncate">{track.track.artists[0].name}</h1>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
