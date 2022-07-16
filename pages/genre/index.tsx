d6b2f3cec9abf8ac6bef487817d3500ffb139a6c
Ahljenn <ahljennm2@gmail.com>
2022-07-15 20:25:27 -0700

    Added working genre search

--- pages/genre/index.tsx
+++ pages/genre/index.tsx
@@ -35,27 +35,39 @@ const Genre: React.FC = (): JSX.Element => {
       <SubpageHeader pageName="Genre" />
       <div className="flex justify-center gap-5 mb-5">
         <input
+          type="text"
           className="bg-gray-200 border-2 text-2xl border-gray-200 rounded w-[20rem] sm:w-[35rem] py-2 px-4 text-gray-700 focus:outline-none focus:bg-white focus:border-pink-500"
           placeholder="Search By Genre"
+          onChange={(e) => setSearchGenre(e.target.value)}
         />
       </div>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:gap-5 mx-5 gap-3">
-        {genres.map((genre: string) => (
-          <div
-            onClick={() => {
-              router.push({
-                pathname: '/genre/[genreResult]',
-                query: { genreId: genre, genreTitle: genre[0].toUpperCase() + genre.slice(1) },
-              });
-            }}
-            className={`cursor-pointer hover:bg-slate-300 hover:text-slate-600 rounded-xl align-start gap-1 h-60 ${
-              colors[Math.floor(Math.random() * colors.length)]
-            }`}
-            key={genre}
-          >
-            <p className="sm:text-2xl m-5">{genre[0].toUpperCase() + genre.slice(1)}</p>
-          </div>
-        ))}
+        {genres
+          .filter((genre: string) => {
+            if (searchGenre === '') {
+              return genre;
+            } else {
+              return genre.toLowerCase().includes(searchGenre.toLowerCase());
+            }
+          })
+          .map((genre: string, key: number) => {
+            return (
+              <div
+                className={`cursor-pointer hover:bg-slate-300 hover:text-slate-600 rounded-xl align-start gap-1 h-60 ${
+                  colors[Math.floor(Math.random() * colors.length)]
+                }`}
+                key={key}
+                onClick={() => {
+                  router.push({
+                    pathname: '/genre/[genreResult]',
+                    query: { genreId: genre, genreTitle: genre[0].toUpperCase() + genre.slice(1) },
+                  });
+                }}
+              >
+                <p className="sm:text-2xl m-5">{genre[0].toUpperCase() + genre.slice(1)}</p>
+              </div>
+            );
+          })}
       </section>
     </>
   );
